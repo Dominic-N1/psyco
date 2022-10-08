724a81d07938524dd11bf367013e946547adb998
Dominic-N1 <vladimirndsm@gmail.com>
2022-10-09 00:40:30 +0700

    add svg support and some scss

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,45 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
+import './scss/main.scss';
+import leafHeartLogo from '/leaf-heart.svg';
+import houseHeart from './img/house-heart.svg';
+import userHairLong from './img/user-vneck-hair-long.svg';
+import comments from './img/comments.svg';
+import simCard from './img/sim-cards.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="./vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <a href="src/about.html">About</a>
+<header>
+  <p class="logo">
+    <img src="${leafHeartLogo}" alt="Leaf Heart" />
+  </p>
+  <div class="menu-btn">
+    <span class="menu-btn_burger"></span>
   </div>
+  <nav class="nav">
+    <ul class="menu-nav">
+      <li class="menu-nav_item active">
+        <a href="index.html" class="menu-nav_link">
+          <img
+            class="heart"
+            src="${houseHeart}"
+            alt="House Heart"
+          />
+        </a>
+      </li>
+      <li class="menu-nav_item">
+        <a href="about.html" class="menu-nav_link">
+          <img src="${userHairLong}" alt="User Hair Long" />
+        </a>
+      </li>
+      <li class="menu-nav_item">
+        <a href="projects.html" class="menu-nav_link">
+          <img src="${comments}" alt="Comments" />
+        </a>
+      </li>
+      <li class="menu-nav_item">
+        <a href="contact.html" class="menu-nav_link">
+          <img src="${simCard}" alt="Sim card" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
